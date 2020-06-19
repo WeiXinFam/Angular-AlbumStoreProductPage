c725ba53d0f04e30d649504f054d5b19127d88d8
Wei Xin Fam <u6554719@anu.edu.au>
2020-06-19 10:13:40 +1000

    Day20

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -6,11 +6,11 @@ import { Http, Response } from '@angular/http'
 @Injectable()
 export class ProductService {
 
-  constructor(private _http: Http) { }
-
   private _albumUrl = '../assets/album.json';
 
-  getAlbum (id:Number){
+  constructor(private _http: Http) { }
+
+  getAlbum (id: number){
     return this._http.get(this._albumUrl).map(response => response.json());
   }
 }
